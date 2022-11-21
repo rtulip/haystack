4f92160f4f902782ce87e741a514ac80487b2109
Robbie Tulip <robbie@brnkl.io>
2022-11-21 10:42:59 -0500

    Separate Type into it's own file

--- src/types/mod.rs
+++ src/types/mod.rs
@@ -1,12 +1,14 @@
 mod framed_type;
 mod function;
 mod record_kind;
+mod r#type;
 mod type_id;
 mod type_info;
 use std::collections::BTreeMap;
 
 pub use framed_type::*;
 pub use function::*;
+pub use r#type::*;
 pub use record_kind::*;
 pub use type_id::*;
 pub use type_info::*;

0892fa32e0a74c76e8b12c234463e03a987b2e91
Robbie Tulip <robbie@brnkl.io>
2023-03-19 13:28:18 -0400

    When checking for variance, ignore tuple idents

--- src/types/variance.rs
+++ src/types/variance.rs
@@ -1,4 +1,4 @@
-use super::{TypeId, TypeMap};
+use super::{Type, TypeId, TypeMap};
 
 #[derive(Debug, PartialEq, Eq, PartialOrd, Ord, Clone, Copy)]
 pub enum Variance {
@@ -10,11 +10,39 @@ pub enum Variance {
 
 impl Variance {
     pub fn new(t1: &TypeId, t2: &TypeId, types: &TypeMap) -> Self {
+        let t1 = match types.get(t1) {
+            Some(Type::Tuple {
+                inner,
+                idents: Some(_),
+            }) => {
+                let t = Type::Tuple {
+                    inner: inner.clone(),
+                    idents: None,
+                };
+                t.id()
+            }
+            _ => t1.clone(),
+        };
+
+        let t2 = match types.get(t2) {
+            Some(Type::Tuple {
+                inner,
+                idents: Some(_),
+            }) => {
+                let t = Type::Tuple {
+                    inner: inner.clone(),
+                    idents: None,
+                };
+                t.id()
+            }
+            _ => t2.clone(),
+        };
+
         if t1 == t2 {
             Variance::Variant
-        } else if t1 == &t2.supertype(types) {
+        } else if t1 == t2.supertype(types) {
             Variance::Contravariant
-        } else if &t1.supertype(types) == t2 {
+        } else if t1.supertype(types) == t2 {
             Variance::Covariant
         } else {
             Variance::Invariant

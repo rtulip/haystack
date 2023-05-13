25722d700fb02fe48e74132e1489f6eb7417cd18
Robbie Tulip <robbie@brnkl.io>
2023-05-13 09:54:00 -0400

    Substitute while and never typed exprs

--- src/ast/expr/typed/while.rs
+++ src/ast/expr/typed/while.rs
@@ -1,7 +1,19 @@
+use crate::{error::HayError, lex::token::Token, types::Substitutions};
+
 use super::TypedExpr;
 
 #[derive(Debug, Clone)]
 pub struct TypedWhileExpr {
     pub cond: Vec<TypedExpr>,
     pub body: Box<TypedExpr>,
+}
+
+impl TypedWhileExpr {
+    pub fn substitute(&mut self, token: &Token, subs: &Substitutions) -> Result<(), HayError> {
+        for e in &mut self.cond {
+            e.substitute(token, subs)?;
+        }
+
+        self.body.substitute(token, subs)
+    }
 }
